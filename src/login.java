28ae5644d5188c7242772a015f1487e637a425a4
Sebastian Boesdal <eaaseboe@students.eaa.dk>
2023-10-05 13:33:36 +0200

    updatereing til login.java

--- src/login.java
+++ src/login.java
@@ -1,5 +1,22 @@
-public class login {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-    }
-}
+public class login {
+            public static void main(String[] args) {
+                // Database-forbindelsesoplysninger
+                String url = "jdbc:mysql://localhost:3306/din_database_navn";
+                String brugernavn = "din_bruger";
+                String adgangskode = "din_adgangskode";
+
+                // Opret forbindelse til databasen
+                try (Connection forbindelse = DriverManager.getConnection(url, brugernavn, adgangskode)) {
+                    System.out.println("Forbindelse til databasen oprettet.");
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
